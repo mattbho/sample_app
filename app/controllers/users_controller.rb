b5a66c4eada1c9fb4987345d5f44770d1f7276b8
Matthew Ho <mtthwbh@gmail.com>
2019-07-08 10:32:21 -0700

    Implement advanced login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,15 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
     end
   end
-
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
